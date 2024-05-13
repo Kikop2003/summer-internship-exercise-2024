37bcebc03b4a422091d1d8bcbf0fe9ad97afde56
Francisco Papoula <fc58206@alunos.fc.ul.pt>
2024-05-13 19:14:53 +0100

    criação de um wrapper para guardar dados necessarios durante uma bfs

--- src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
+++ src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
@@ -2,15 +2,51 @@ package com.premiumminds.internship.teknonymy;
 
 import com.premiumminds.internship.teknonymy.Person;
 
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
 class TeknonymyService implements ITeknonymyService {
 
-  /**
-   * Method to get a Person Teknonymy Name
-   * 
-   * @param Person person
-   * @return String which is the Teknonymy Name 
-   */
-  public String getTeknonymy(Person person) {
-    throw new RuntimeException("Not Implemented Yet");
-  };
+    /**
+     * Method to get a Person Teknonymy Name
+     * 
+     * @param Person person
+     * @return String which is the Teknonymy Name
+     */
+    public String getTeknonymy(Person person) {
+        if (person.children().length == 0) {
+            return "";
+        }
+        LinkedList<AuxWrapper> list = bfs(person);
+    }
+
+    private LinkedList<AuxWrapper> bfs(Person person) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'bfs'");
+    }
+    
+    private class AuxWrapper {
+        private int i;
+        private Person person;
+        private LocalDateTime dateOfBirth;
+
+        public AuxWrapper(Person person, int i) {
+            this.person = person;
+            this.i = i;
+        }
+
+        public int getInt() {
+            return i;
+        }
+
+        public Person getPerson() {
+            return person;
+        }
+
+        public LocalDateTime getDateOfBirth() {
+            return dateOfBirth;
+        }
+
+    }
+
 }
